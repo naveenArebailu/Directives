2cfda2f84591f81c7dc012532ab6ee15857eb0c8
naveen arebailu <naveen.arebail@gmail.com>
2017-09-09 23:01:46 +0530

    files added

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title: string;
+  firstName:string;
+  lastName:string;
+  clicked : boolean = false;
+
+ showtitle(){
+   this.clicked=true;
+    this.title=`${this.firstName} ${this.lastName}`;
+ }
+}
